d3f45816560fa8128341869da808b28cdebfe6ed
sakibhasan997 <sssakibhasan997@gmail.comm>
2023-06-10 19:03:41 +0600

    jani na ki korsi

--- index.js
+++ index.js
@@ -58,6 +58,24 @@ async function run() {
     // })
 
 
+    // cart collection apis
+    app.get('/carts', async(req, res)=>{
+      const email = req.query.email;
+      if(!email){
+        res.send([]);
+      }
+      const query = {email: email};
+      const result = await cartCollection.find(query).toArray();
+      res.send(result)
+    });
+    app.post('/carts', async(req, res)=>{
+      const item = req.body;
+      console.log(item);
+      const result = await cartCollection.insertOne(item);
+      res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
