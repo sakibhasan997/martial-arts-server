0509a83439293d94e7481fb2e4b3d22125924247
sakibhasan997 <sssakibhasan997@gmail.comm>
2023-06-11 01:50:14 +0600

    jwt token set

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ async function run() {
     // jwt
     app.post('/jwt', (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '2 day' }) // TODO: 1h
 
       res.send({ token })
     })
