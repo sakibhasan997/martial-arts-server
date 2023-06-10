8e63f4c413f2a2be8edd6a82c846698a86f9db02
sakibhasan997 <sssakibhasan997@gmail.comm>
2023-06-10 13:13:07 +0600

    get classes

--- index.js
+++ index.js
@@ -25,43 +25,43 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const classesCollection = client.db('martialDB').collection('classes');
     const instructorCollection = client.db('martialDB').collection('instructors');
 
     app.get('/classes', async (req, res) => {
-        const classes = await classesCollection.find().sort({
-            student_enroll: -1
-        }).toArray();
-        res.send(classes);
+      const result = await classesCollection.find().sort({
+        studentEnroll: -1
+      }).toArray();
+      res.send(result);
     })
+
 
     app.get('/top-classes', async (req, res) => {
         const classes = await classesCollection.find().limit(6).sort({
-            student_enroll: -1
+            studentEnroll: -1
         }).toArray();
         res.send(classes);
     })
 
-
-    // Instructor api
-    app.get('/all-instructor', async (req, res) => {
-        const instructor = await instructorCollection.find().toArray();
-        res.send(instructor);
-    })
-    app.get('/instructor', async (req, res) => {
-        const instructor = await instructorCollection.find().limit(6).toArray();
-        res.send(instructor);
-    })
+    // // Instructor api
+    // app.get('/all-instructor', async (req, res) => {
+    //     const instructor = await instructorCollection.find().toArray();
+    //     res.send(instructor);
+    // })
+    // app.get('/instructor', async (req, res) => {
+    //     const instructor = await instructorCollection.find().limit(6).toArray();
+    //     res.send(instructor);
+    // })
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
@@ -69,10 +69,10 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Hello Brother I am Running Now You Can Do This')
-  });
-  
-  
-  app.listen(port, () => {
-    console.log(`server site is running port: ${port}`);
-  })
+  res.send('Hello Brother I am Running Now You Can Do This')
+});
+
+
+app.listen(port, () => {
+  console.log(`server site is running port: ${port}`);
+})
